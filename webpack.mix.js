086532f75adbd967bbd9b1a183bf0a72a3062ee8
Phil Stephens <theprivateer@users.noreply.github.com>
2024-10-17 12:54:17 +1000

    Actually include the jigsaw() build command 🤦

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,7 +4,7 @@ require('laravel-mix-jigsaw');
 mix.disableSuccessNotifications();
 mix.setPublicPath('source/assets/build');
 
-// mix.jigsaw()
+mix.jigsaw();
 //     .js('source/_assets/js/main.js', 'js')
 //     .css('source/_assets/css/main.css', 'css', [
 //         require('postcss-import'),
