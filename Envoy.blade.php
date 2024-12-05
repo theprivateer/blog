7e7ca377bd929464b07e1a47346114fd0590679a
Phil Stephens <phils@hey.com>
2024-12-05 22:07:09 +1000

    Run deployment using Envoy

---
+++ Envoy.blade.php
@@ -0,0 +1,8 @@
+@servers(['web' => ['phil@10.126.0.2']
+
+@task('deploy', ['on' => 'web'])
+    cd /var/www/philstephens.com
+    git pull
+    composer install
+    npm run build
+@endtask
--- app/Http/Controllers/GitHubWebhookController.php
+++ app/Http/Controllers/GitHubWebhookController.php
@@ -12,19 +12,12 @@ class GitHubWebhookController extends Controller
     public function __invoke(Request $request)
     {
         // TODO: Authorization logic
-
-
-        info($request->getContent());
-
         $push = json_decode($request->getContent());
-
-        info($push->ref);
-        info($push->repository->full_name);
 
         if ($push->ref == 'refs/heads/main'
             && $push->repository->full_name == 'theprivateer/blog') {
                 info('Running git pull');
-                $result = Process::path(base_path())->run('git pull');
+                $result = Process::path(base_path())->run('php vendor/bin/envoy run deploy');
                 info($result->output());
         }
 
