0b068a613120c5511b0155c99a77e09ff7a22ecc
Phil Stephens <phils@hey.com>
2024-12-05 19:25:10 +1000

    Attempt to run git pull on webhoot trigger

--- app/Http/Controllers/GitHubWebhookController.php
+++ app/Http/Controllers/GitHubWebhookController.php
@@ -3,12 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Process\Pipe;
+use Illuminate\Process\ProcessResult;
+use Illuminate\Support\Facades\Process;
 
 class GitHubWebhookController extends Controller
 {
     public function __invoke(Request $request)
     {
-        info($request->getContent());
+        // TODO: Authorization logic
+
+
+        $push = json_decode($request->getContent());
+
+        if ($push->ref == 'refs/heads/main'
+            && $push->repository->full_name == 'theprivateer/blog') {
+                Process::path(base_path())->run('git pull');
+        }
 
         return;
     }
