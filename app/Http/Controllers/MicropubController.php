6cdc2d07676bd505470a8d868e11fafae0fad115
Phil Stephens <phils@hey.com>
2024-12-11 06:12:03 +1000

    Actually fire the PostPublished event

--- app/Http/Controllers/MicropubController.php
+++ app/Http/Controllers/MicropubController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use App\Events\PostPublished;
+use Illuminate\Http\JsonResponse;
+use Spatie\Sheets\Facades\Sheets;
 use Illuminate\Support\Facades\Storage;
 
 class MicropubController extends Controller
@@ -19,13 +21,13 @@ class MicropubController extends Controller
     {
         // @TODO: Refactor to a service
 
-        // Process the post payload
+        // Process the post payload and set default values
         $data = json_decode($request->getContent());
         $title = $data->properties->name[0];
         $body = trim($data->properties->content[0]);
         $link = null;
 
-        // is it a link
+        // Break up the body to examine the first couple of lines
         $lines = explode("\n", $body);
 
         // Check for an inline title
@@ -34,7 +36,10 @@ class MicropubController extends Controller
             $title = ltrim($title, '# ');
         }
 
-        // Check for a link within the first line or two
+        // Check for a link either on the first line,
+        // or second if there was an inline title
+        // iA Writer encloses URLs in <> when publishing via Micropub
+        // A regex would be more elegant here, but this does the job
         if (strpos($lines[0], '<http') === 0) {
             $link = array_shift($lines);
             $link = ltrim($link, '<');
@@ -45,6 +50,8 @@ class MicropubController extends Controller
         $body = implode("\n", $lines);
 
         // Generate the frontmatter
+        // Note: even if frontmatter is set in the document
+        // iA Writer will not send it through in the payload
         $content = ['---'];
         $content[] = 'title: "' . $title . '"';
 
@@ -55,14 +62,21 @@ class MicropubController extends Controller
         $content[] = '---';
         $content[] = $body;
 
-        $filename = now()->format('Y-m-d\TH:i:s') . '.' . Str::slug($title) . '.md';
+        $filename = now()->format('Y-m-d\TH:i:s') . '.' . Str::of($title)->slug();
 
-        Storage::disk('posts')->put($filename, implode("\n", $content));
+        Storage::disk('posts')->put(
+            $filename . '.' . config('sheets.collections.posts.extension'),
+            implode("\n", $content)
+        );
+
+        $post = Sheets::collection('posts')->get($filename);
+
+        event(new PostPublished($post));
 
         return response(
             null,
             201,
-            ['Location' => route('posts.show', Str::slug($title))]
+            ['Location' => route('posts.show', $post->slug)]
         );
     }
 }
