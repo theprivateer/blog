33025a1f6c3522bb54c5d133048b6b35ebe4cc5a
Phil Stephens <phils@hey.com>
2024-12-18 06:26:11 +1000

    Tidy up unused imports

--- app/ContentParsers/MarkdownWithFrontMatterParser.php
+++ app/ContentParsers/MarkdownWithFrontMatterParser.php
@@ -4,36 +4,15 @@ namespace App\ContentParsers;
 
 use Spatie\Sheets\ContentParser;
 use Illuminate\Support\HtmlString;
-use League\CommonMark\MarkdownConverter;
-use League\CommonMark\CommonMarkConverter;
 use Spatie\YamlFrontMatter\YamlFrontMatter;
-use League\CommonMark\Environment\Environment;
-use League\CommonMark\Extension\GithubFlavoredMarkdownExtension;
-use League\CommonMark\Extension\CommonMark\CommonMarkCoreExtension;
-use League\CommonMark\Extension\Footnote\FootnoteExtension;
 use Spatie\LaravelMarkdown\MarkdownRenderer;
 
 class MarkdownWithFrontMatterParser implements ContentParser
 {
-    /** @var \League\CommonMark\CommonMarkConverter */
-    // protected $commonMarkConverter;
-
-    // public function __construct(CommonMarkConverter $commonMarkConverter)
-    // {
-    //     $this->commonMarkConverter = $commonMarkConverter;
-    // }
-
     public function parse(string $contents): array
     {
         $document = YamlFrontMatter::parse($contents);
 
-        // $config = [];
-        // $environment = new Environment($config);
-        // $environment->addExtension(new CommonMarkCoreExtension());
-        // $environment->addExtension(new GithubFlavoredMarkdownExtension());
-        // $environment->addExtension(new FootnoteExtension());
-
-        // $converter = new MarkdownConverter($environment);
         $htmlContents = app(MarkdownRenderer::class)->toHtml($document->body());
 
         return array_merge(
