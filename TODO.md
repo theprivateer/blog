46ee9ba723d0b5ab9881b74b9f263877ce4f1163
Phil Stephens <phils@hey.com>
2025-01-18 09:09:17 +1000

    Move Micropub logic into service wrapper

--- TODO.md
+++ TODO.md
@@ -4,17 +4,35 @@ An non-exhaustive list of features I want to add to this site over time (in no p
 
 - [x] Get Markdown footnotes rendering properly
 - [ ] Parse Markdown in update and callout frontmatter
-- [ ] SEO tags
+- [ ] SEO tags, including JSON-LD
 - [x] TOC parsing and display for posts
     - [x] Styling for headings inside of posts / pages
 - [x] Sitemap (for better indexing)
 - [ ] Better image optimisation with Glide
 - [ ] Refactor Micropub publishing into a service
 - [ ] Publish nested slash pages
-- [ ] Update existing posts
+- [ ] Update existing posts via micropub
+- [ ] Block AI crawling
+- [ ] Custom parser for call-outs
+- [ ] Pagination
+- [ ] Separate RSS feeds for articles and links
 
 More /slash pages - https://slashpages.net
 - [x] /uses
 - [x] /colophon
 - [x] /defaults
 - [x] /follow or /subscribe or /feeds
+- [ ] /blogroll
+- [ ] /podroll
+- [ ] /favourites and /favorites
+- [ ] /someday
+- [ ] /reading
+- [ ] /changelog
+- [ ] /ai
+- [ ] /interests
+- [ ] /carry
+- [ ] /yep
+- [ ] /why
+- [ ] /til
+- [ ] /nope
+- [ ] /ideas
--- app/Http/Controllers/MicropubController.php
+++ app/Http/Controllers/MicropubController.php
@@ -2,152 +2,25 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Support\Str;
 use Illuminate\Http\Request;
-use App\Events\PostPublished;
+use App\Services\MicropubService;
 use Illuminate\Http\Response;
 use Illuminate\Http\JsonResponse;
-use Spatie\Sheets\Facades\Sheets;
-use Illuminate\Support\Facades\Storage;
 
 class MicropubController extends Controller
 {
+    public function __construct(public MicropubService $service)
+    {
+        //
+    }
+
     public function getCapabilities(Request $request): JsonResponse
     {
-        // return empty JSON object with 200 status code
-        return response()->json(null);
+        return $this->service->getCapabilities($request);
     }
 
     public function publish(Request $request): Response
     {
-        // @TODO: Refactor to a service
-
-        if ($request->hasFile('file')) {
-            return $this->processImage($request);
-        }
-
-        // Process the post payload and set default values
-        $data = json_decode($request->getContent());
-        $title = $data->properties->name[0];
-        $body = trim($data->properties->content[0]);
-        $link = null;
-
-        // Determine whether this is a post or slashpage and handle accordingly
-        $slashes = Sheets::collection('slashes')
-                        ->all()
-                        ->pluck('slug')
-                        ->all();
-
-        if (in_array($title, $slashes)) {
-            return $this->updateSlashPage($title, $body);
-        }
-
-        // Break up the body to examine the first couple of lines
-        $lines = explode("\n", $body);
-
-        // Check for an inline title
-        if (strpos($lines[0], '# ') === 0) {
-            $title = array_shift($lines);
-            $title = ltrim($title, '# ');
-        }
-
-        // Check for a link either on the first line, or second if
-        // there was an inline title
-        // iA Writer encloses URLs in <> when publishing via Micropub
-        // A regex would be more elegant here, but this does the job
-        if (strpos($lines[0], '<http') === 0) {
-            $link = array_shift($lines);
-            $link = ltrim($link, '<');
-            $link = rtrim($link, '>');
-        }
-
-        // The rest is the body - stick it back together
-        $body = implode("\n", $lines);
-
-        // Generate the frontmatter
-        // Note: even if frontmatter is set in the document
-        // iA Writer will not send it through in the payload
-        $content = ['---'];
-        $content[] = 'title: "' . $title . '"';
-
-        if ($link) {
-            $content[] = "link: {$link}";
-        }
-
-        $content[] = '---';
-        $content[] = $body;
-
-        $filename = now()->format('Y-m-d\TH:i:s') . '.' . Str::of($title)->slug();
-
-        Storage::disk('posts')->put(
-            $filename . '.' . config('sheets.collections.posts.extension'),
-            implode("\n", $content)
-        );
-
-        $post = Sheets::collection('posts')->get($filename);
-
-        event(new PostPublished($post));
-
-        return response(
-            null,
-            201,
-            ['Location' => route('posts.show', $post->slug)]
-        );
-    }
-
-    private function updateSlashPage(string $filename, string $body): Response
-    {
-        $title = ucwords($filename);
-
-        // Break up the body to examine the first couple of lines
-        $lines = explode("\n", $body);
-
-        // Check for an inline title
-        if (strpos($lines[0], '# ') === 0) {
-            $title = array_shift($lines);
-            $title = ltrim($title, '# ');
-        }
-
-        // The rest is the body - stick it back together
-        $body = implode("\n", $lines);
-
-        // Generate the frontmatter
-        // Note: even if frontmatter is set in the document
-        // iA Writer will not send it through in the payload
-        $content = ['---'];
-        $content[] = 'title: "' . $title . '"';
-        $content[] = 'modified: ' . now()->format('Y-m-d\TH:i:s');
-        $content[] = '---';
-        $content[] = $body;
-
-        Storage::disk('slashes')->put(
-            $filename . '.' . config('sheets.collections.posts.extension'),
-            implode("\n", $content)
-        );
-
-        $post = Sheets::collection('slashes')->get($filename);
-
-        event(new PostPublished($post));
-
-        return response(
-            null,
-            201,
-            ['Location' => route('slashes.show', $post->slug)]
-        );
-    }
-
-    private function processImage(Request $request): Response
-    {
-        $path = $request->file->storeAs(
-            'images/' . now()->format('Y/m'),
-            $request->file->getClientOriginalName(),
-            'public'
-        );
-
-        return response(
-            null,
-            201,
-            ['Location' => asset('storage/' . $path)]
-        );
+        return $this->service->processRequest($request);
     }
 }
