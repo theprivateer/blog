faeb2ce7121279e79cc6e9209f451508b9b2de49
Phil Stephens <phils@hey.com>
2025-01-18 17:49:12 +1000

    Cleanup

--- app/Services/MicropubService.php
+++ app/Services/MicropubService.php
@@ -34,13 +34,19 @@ class MicropubService
         // is it a slash page?
         foreach ($lines as $line) {
             if (strpos($line, '# ') === 0) {
+                // Check for an inline title
                 $title = ltrim($line, '# ');
                 array_shift($lines);
             } elseif (strpos($line, '<http') === 0) {
+                // Check for a link either on the first line, or second if
+                // there was an inline title
+                // iA Writer encloses URLs in <> when publishing via Micropub
+                // A regex would be more elegant here, but this does the job
                 $link = ltrim($line, '<');
                 $link = rtrim($link, '>');
                 array_shift($lines);
             } elseif (strpos($line, '/') === 0) {
+                // Check for a parent path (for nested slash pages)
                 $path = $line;
                 array_shift($lines);
             } else {
@@ -79,80 +85,13 @@ class MicropubService
             title: $title ?? null,
             link: $link ?? null
         );
-
-
-
-        // Process the post payload and set default values
-
-        // $title = $data->properties->name[0];
-        // $body = trim($data->properties->content[0]);
-        // $link = null;
-
-        // // Determine whether this is a post or slashpage and handle accordingly
-        // $slashes = Sheets::collection('slashes')
-        //                 ->all()
-        //                 ->pluck('slug')
-        //                 ->all();
-
-        // if (in_array($title, $slashes)) {
-        //     return $this->updateSlashPage($title, $body);
-        // }
-
-        // Break up the body to examine the first couple of lines
-        // $lines = explode("\n", $body);
-
-        // Check for an inline title
-        // if (strpos($lines[0], '# ') === 0) {
-        //     $title = array_shift($lines);
-        //     $title = ltrim($title, '# ');
-        // }
-
-        // Check for a link either on the first line, or second if
-        // there was an inline title
-        // iA Writer encloses URLs in <> when publishing via Micropub
-        // A regex would be more elegant here, but this does the job
-        // if (strpos($lines[0], '<http') === 0) {
-        //     $link = array_shift($lines);
-        //     $link = ltrim($link, '<');
-        //     $link = rtrim($link, '>');
-        // }
-
-        // The rest is the body - stick it back together
-        // $body = implode("\n", $lines);
-
-        // Generate the frontmatter
-        // Note: even if frontmatter is set in the document
-        // iA Writer will not send it through in the payload
-        // $content = ['---'];
-        // $content[] = 'title: "' . $title . '"';
-
-        // if ($link) {
-        //     $content[] = "link: {$link}";
-        // }
-
-        // $content[] = '---';
-        // $content[] = $body;
-
-        // $filename = now()->format('Y-m-d\TH:i:s') . '.' . Str::of($title)->slug();
-
-        // Storage::disk('posts')->put(
-        //     $filename . '.' . config('sheets.collections.posts.extension'),
-        //     implode("\n", $content)
-        // );
-
-        // $post = Sheets::collection('posts')->get($filename);
-
-        // event(new PostPublished($post));
-
-        // return response(
-        //     null,
-        //     201,
-        //     ['Location' => route('posts.show', $post->slug)]
-        // );
     }
 
     private function updatePost(string $filename, string $body, ?string $title = null, ?string $link = null): Response
     {
+        // Generate the frontmatter
+        // Note: even if frontmatter is set in the document
+        // iA Writer will not send it through in the payload
         $content = ['---'];
         if ($title) {
             $content[] = 'title: "' . $title . '"';
@@ -161,8 +100,7 @@ class MicropubService
             $content[] = "link: {$link}";
         }
 
-        // Does the file exist?
-
+        // Does the file already exist?
         $exists = Sheets::collection('posts')
             ->all()
             ->where('slug', $filename)
@@ -199,18 +137,6 @@ class MicropubService
     private function updateSlashPage(string $filename, string $body, ?string $title = null, ?string $path = null): Response
     {
         $title = $title ?: ucwords($filename);
-info('Updating slash page');
-        // Break up the body to examine the first couple of lines
-        // $lines = explode("\n", $body);
-
-        // // Check for an inline title
-        // if (strpos($lines[0], '# ') === 0) {
-        //     $title = array_shift($lines);
-        //     $title = ltrim($title, '# ');
-        // }
-
-        // The rest is the body - stick it back together
-        // $body = implode("\n", $lines);
 
         // Generate the frontmatter
         // Note: even if frontmatter is set in the document
@@ -222,12 +148,12 @@ info('Updating slash page');
         $content[] = $body;
 
         $filename = ($path) ? ltrim($path, '/') . '/' . $filename : $filename;
-info($filename);
-$stored =        Storage::disk('slashes')->put(
+
+        Storage::disk('slashes')->put(
             $filename . '.' . config('sheets.collections.posts.extension'),
             implode("\n", $content)
         );
-info($stored);
+
         $post = Sheets::collection('slashes')->get($filename);
 
         event(new PostPublished(post: $post));
@@ -247,6 +173,7 @@ info($stored);
             'public'
         );
 
+        // @TODO: implement Glide for image optimisation and return a Glide URL here
         return response(
             null,
             201,
